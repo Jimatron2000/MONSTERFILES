002aa3ad51f6902b8016b982badec9f8969fb3c7
Jimatron2000 <151482107+Jimatron2000@users.noreply.github.com>
2023-11-20 10:11:24 -0500

    wait until page loads

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var MAKEYOURCHOICE = 'elcome Agent, input the login quickly and precisely'
 var WHENWILLYOUFACEUS = 0;
-function FINDOUTTHETRUTH() {
-    if (WHENWILLYOUFACEUS < MAKEYOURCHOICE.length) {
-        document.getElementById("DECISIONS").innerText += MAKEYOURCHOICE.charAt(WHENWILLYOUFACEUS);
-        WHENWILLYOUFACEUS++;
-        setTimeout(FINDOUTTHETRUTH, 50);
-    }
- }
-FINDOUTTHETRUTH();
+document.addEventListener('DOMContentLoaded', function() {
+    function FINDOUTTHETRUTH() {
+        if (WHENWILLYOUFACEUS < MAKEYOURCHOICE.length) {
+            document.getElementById("DECISIONS").innerText += MAKEYOURCHOICE.charAt(WHENWILLYOUFACEUS);
+            WHENWILLYOUFACEUS++;
+            setTimeout(FINDOUTTHETRUTH, 50);
+        }
+     }
+     FINDOUTTHETRUTH()
+  });
