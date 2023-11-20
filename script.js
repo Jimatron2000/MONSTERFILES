ad7d601c3fc326c86ba1a480710df6c9034525fb
Jimatron2000 <151482107+Jimatron2000@users.noreply.github.com>
2023-11-20 10:02:52 -0500

    SOMETIME YOU WILL LeARN

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 var MAKEYOURCHOICE = 'Welcome Agent, input correctly and precisely.'
 var WHENWILLYOUFACEUS = 0;
-var hasran = true;
 function FINDOUTTHETRUTH() {
-    if (WHENWILLYOUFACEUS < MAKEYOURCHOICE.length && hasran == true) {
+    if (WHENWILLYOUFACEUS < MAKEYOURCHOICE.length) {
         document.getElementById("DECISIONS").innerHTML += MAKEYOURCHOICE.charAt(i);
         WHENWILLYOUFACEUS++;
         setTimeout(FINDOUTTHETRUTH, 50);
-    } else {
-        hasran = false;
     }
  }
 FINDOUTTHETRUTH();
